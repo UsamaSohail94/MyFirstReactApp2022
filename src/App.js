d8ccc7cc32731d6f7c3e0168adcd70a1ebcaa7b1
Muhammad Usama Sohail <37038343+UsamaSohail94@users.noreply.github.com>
2022-08-11 01:04:39 +0500

    Update Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        My First Reat App!
+       Hello From Usama!!
       </header>
     </div>
   );
